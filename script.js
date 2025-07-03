1aa475d208cd8e380e58143c344941f889a8b025
natancappra <131809962+natancappra@users.noreply.github.com>
2025-07-03 14:56:11 -0300

    mudanças de design

--- script.js
+++ script.js
@@ -2,8 +2,19 @@
 const menuHamburger = document.getElementById('menu-hamburger');
 const navMenu = document.getElementById('nav-menu');
 
-// Adiciona um "ouvinte de evento" de clique no ícone do hambúrguer
+// NOVO: Seleciona todos os links dentro do menu de navegação
+const navLinks = document.querySelectorAll('#nav-menu a');
+
+// Adiciona um "ouvinte de evento" de clique no ícone três traços
 menuHamburger.addEventListener('click', () => {
     // A cada clique, ele adiciona ou remove a classe 'active' do menu
     navMenu.classList.toggle('active');
+});
+
+// NOVO: Adiciona um "ouvinte" para cada link do menu
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        // Quando um link é clicado, remove a classe 'active' para fechar o menu
+        navMenu.classList.remove('active');
+    });
 });
